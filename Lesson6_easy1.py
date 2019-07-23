6b6a0601836bdc62a338cffb6726bd3210019233
JimGilmour <52409169+JimGilmour@users.noreply.github.com>
2019-07-23 14:28:03 +0300

    Add files via upload

---
+++ Lesson6_easy1.py
@@ -0,0 +1,129 @@
+# Задача - 1
+# Опишите несколько классов TownCar, SportCar, WorkCar, PoliceCar
+# У каждого класса должны быть следующие аттрибуты:
+# speed, color, name, is_police - Булево значение.
+# А так же несколько методов: go, stop, turn(direction) - которые должны сообщать,
+#  о том что машина поехала, остановилась, повернула(куда)
+
+
+class TownCar:
+
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print('{} is going!'.format(self.name))
+
+    def stop(self):
+        print('{} is stopping!'.format(self.name))
+
+    def turn(self, direction):
+        print('{} is turning to {}!'.format(self.name, direction))
+
+
+class SportCar:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print('{} is going!'.format(self.name))
+
+    def stop(self):
+        print('{} is stopping!'.format(self.name))
+
+    def turn(self, direction):
+        print('{} is turning to {}!'.format(self.name, direction))
+
+
+class WorkCar:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print('{} is going!'.format(self.name))
+
+    def stop(self):
+        print('{} is stopping!'.format(self.name))
+
+    def turn(self, direction):
+        print('{} is turning to {}!'.format(self.name, direction))
+
+
+class PoliceCar:
+    def __init__(self, speed, color, name):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = True
+
+    def go(self):
+        print('{} is going!'.format(self.name))
+
+    def stop(self):
+        print('{} is stopping!'.format(self.name))
+
+    def turn(self, direction):
+        print('{} is turning to {}!'.format(self.name, direction))
+
+
+# Задача - 2
+# Посмотрите на задачу-1 подумайте как выделить общие признаки классов
+# в родительский и остальные просто наследовать от него.
+
+class Car:
+    def __init__(self, speed, color, name):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = False
+
+    def go(self):
+        print('{} is going!'.format(self.name))
+
+    def stop(self):
+        print('{} is stopping!'.format(self.name))
+
+    def turn(self, direction):
+        print('{} is turning to {}!'.format(self.name, direction))
+
+
+class TownCar(Car):
+    pass
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, has_turbo=True):
+        super().__init__(speed, color, name)
+        self.has_turbo = has_turbo
+
+
+class WorkCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name):
+        super().__init__(speed, color, name)
+        self.is_police = True
+
+
+sportCar = SportCar(220, 'black', 'sport')
+townCar = TownCar(160, 'yellow', 'town')
+workCar = WorkCar(100, 'red', 'work')
+policeCar = PoliceCar(200, 'white', 'police')
+
+
+print(policeCar.is_police)
+
+for i in sportCar:
+ print(i)
+
